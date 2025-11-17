5e00b0c421f1a9e9b936977bf0bc6f761ab0badb
Tuan Son Ca <cason101105@gmail.com>
2025-11-17 22:36:59 +0700

    Add files via upload

---
+++ code/main.cpp
@@ -0,0 +1,12 @@
+// ------------------------------------------------------------------------
+// main.cpp							(c)1997 Jesse Hall. All rights reserved.
+// ------------------------------------------------------------------------
+
+#include "CPadApp.h"
+
+int main( void ) {
+	new CPadApp;
+	be_app->Run();
+	delete be_app;
+	return 0;
+}
---
+++ headers/Pad.h
@@ -0,0 +1,12 @@
+const uint32	msg_Next		= 1;
+const uint32	msg_Prev		= 2;
+const uint32	msg_New			= 3;
+const uint32	msg_Del			= 4;
+const uint32	msg_PadClosed	= 5;
+const uint32	msg_Open		= 6;
+const uint32	msg_Save		= 7;
+
+const char		kInfoAttr[]		= "info";
+const char		kPagesAttr[]	= "pages";
+
+const char		kPadMimeType[]	= "text/x-notepad";
