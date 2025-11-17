c974139ac58931f6d77ffb4b62c6571dabf79dbe
Tuan Son Ca <cason101105@gmail.com>
2025-11-17 23:29:00 +0700

    Update Pad.h

--- headers/Pad.h
+++ headers/Pad.h
@@ -1,12 +1,25 @@
-const uint32	msg_Next		= 1;
-const uint32	msg_Prev		= 2;
-const uint32	msg_New			= 3;
-const uint32	msg_Del			= 4;
-const uint32	msg_PadClosed	= 5;
-const uint32	msg_Open		= 6;
-const uint32	msg_Save		= 7;
+#ifndef PAD_H
+#define PAD_H
 
-const char		kInfoAttr[]		= "info";
-const char		kPagesAttr[]	= "pages";
+// Định nghĩa các hằng số tin nhắn (Message Constants)
+// Các giá trị 'xxxx' là quy ước của BeOS để tạo mã uint32 duy nhất
 
-const char		kPadMimeType[]	= "text/x-notepad";
+// Tin nhắn cho Menu Tệp
+const uint32 msg_Open = 'mOpn';
+const uint32 msg_Save = 'mSav';
+
+// Tin nhắn cho Menu Ghi chú (Note)
+const uint32 msg_Next = 'mNxt';
+const uint32 msg_Prev = 'mPrv';
+const uint32 msg_New = 'mNew';
+const uint32 msg_Del = 'mDel';
+
+// Tin nhắn nội bộ của ứng dụng
+const uint32 msg_PadClosed = 'mPcl';
+
+// Định nghĩa các hằng số thuộc tính (Attribute Constants)
+const char *kPadMimeType = "application/x-jessehall-pad";
+const char *kInfoAttr = "Pad:info";
+const char *kPagesAttr = "Pad:pages";
+
+#endif // PAD_H
